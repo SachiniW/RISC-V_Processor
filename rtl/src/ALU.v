29f186361b81f48bb5c568d3eb69f95a0d7e427c
SachiniW <sswickramasinghe@gmail.com>
2021-10-31 19:20:37 +0530

    Modify the ALU unit
    -Did changes in alu_out and modified the code

--- rtl/src/ALU.v
+++ rtl/src/ALU.v
@@ -24,32 +24,23 @@ module ALU(
     input [31:0] in1, //rs1
     input [31:0] in2, //rs2 or imm 
     input [3:0] alu_op, // inst[] 
-    output reg [31:0] alu_out
+    output [31:0] alu_out
     );
 
-	always @(*)
-		begin
-			case(alu_op)
-				4'd0: alu_out <= in1 + in2; //ADD
-				4'd1: alu_out <= in1 - in2; //SUB
-				4'd2: alu_out <= in1 & in2; //AND
-				4'd3: alu_out <= in1 | in2; //OR
-				4'd4: alu_out <= in1 ^ in2; //XOR
-			    4'd5:begin //SLT
-			        if ($signed(in1) < $signed(in2)) alu_out <= 31'd1; 
-			        else           alu_out <= 31'd0;     
-			        end 
-				4'd6: alu_out <= in1 << in2[4:0]; //SLL
-				4'd7:begin //SLTU
-			        if (in1 < in2) alu_out <= 31'd1; 
-                    else           alu_out <= 31'd0;     
-                    end 								
-				4'd8: alu_out <= in1 >> in2[4:0]; //SRL
-				4'd9: alu_out <= $signed(in1[31:0]) >>> in2[4:0]; //SRA
-                4'd10: alu_out <= in1; //COPY in1
-                4'd11: alu_out <= in2; //COPY in2  				
-				default: alu_out <= alu_out;
-			endcase
-		end	
-		
+
+    assign alu_out = (alu_op  == 4'd0)? in1+in2: //ADD
+                     (alu_op  == 4'd1)? in1-in2: //SUB
+                     (alu_op  == 4'd2)? in1&in2: //AND
+                     (alu_op  == 4'd3)? in1|in2: //OR
+                     (alu_op  == 4'd4)? in1^in2: //XOR
+                     ((alu_op == 4'd5)&($signed(in1) < $signed(in2)))? 31'd1: //SLT
+                     (alu_op  == 4'd6)? in1 << in2[4:0]: //SLL
+                     ((alu_op == 4'd7)&(in1 < in2))? 31'd1: //SLTU
+                     (alu_op  == 4'd8)? in1 >> in2[4:0]: //SRL
+                     (alu_op  == 4'd9)? $signed(in1[31:0]) >>> in2[4:0]: //SRA
+                     (alu_op == 4'd10)? in1: //COPY in1
+                     (alu_op == 4'd11)? in2: //COPY in2  
+                      32'd0;
+
+	
 endmodule
